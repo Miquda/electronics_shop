3f30efec7696ee828658f7e18eb75feeb843b021
mihailkudymov <mike.kudim@gmail.com>
2023-02-21 11:41:48 +0200

    Написан класс Product со всеми требованиями к классу, в модуле main написано создание экземляров и вызов методов

---
+++ classes.py
@@ -0,0 +1,19 @@
+class Product:
+    discount_price = 0.85
+    product_list =[]
+
+    def __init__(self, product_name, price, amount):
+        self.product_name = product_name
+        self.price = price
+        self.amount = amount
+        self.product_list.append(self)
+
+    def get_total_amount(self):
+        return self.price * self.amount
+
+    def set_discount(self):
+        return self.price * self.discount_price
+
+    def __repr__(self):
+        return (f'Product - {self.product_name}, price - {self.price}, amount - {self.amount}')
+
--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from classes import Product
 
+item1 = Product('Iphone', 1200, 15)
+item2 = Product('Google Pixel', 1000, 10)
+
+print(item1.get_total_amount())
+print(item2.get_total_amount())
+
+Product.discount_price = 0.8
+
+print(item1.set_discount())
+print(item2.set_discount())
+
+print(Product.product_list)
